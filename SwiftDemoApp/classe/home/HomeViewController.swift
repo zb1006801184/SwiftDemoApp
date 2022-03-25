573e59762aff5a7fe01cf533c7d67eba096f9b29
zhubiao02 <zhubiao02@58.com>
2022-03-25 14:08:09 +0800

    添加启动图

---
+++ SwiftDemoApp/classe/home/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  SwiftDemoApp
+//
+//  Created by zhubiao on 2022/3/25.
+//
+
+import Foundation
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "首页"
+    }
+}
