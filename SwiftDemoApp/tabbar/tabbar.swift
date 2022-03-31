f330cc042cb9bbf540463f4595b93229e69cc4d6
zhubiao02 <zhubiao02@58.com>
2022-03-31 14:08:28 +0800

    加载SVG图片，添加四个tabbar

--- SwiftDemoApp/tabbar/tabbar.swift
+++ SwiftDemoApp/tabbar/tabbar.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import UIKit
+import SwiftHEXColors
+import SVGKit
 
 class TabBarViewController: UITabBarController {
     
@@ -21,20 +23,41 @@ class TabBarViewController: UITabBarController {
     func createViewController() {
         var tabArray:[UINavigationController] = []
         tabArray.append(createNav(HomeViewController(), title: "首页", image: "tabbar_home", seletImage: "tabbar_home_select.png"))
-        tabArray.append(createNav(MenuViewController(), title: "菜单", image: "tabbar_home", seletImage: "tabbar_home_select.png"))
+        tabArray.append(createNav(MenuViewController(), title: "菜单", image: "tabbar_menu.svg", seletImage: "tabbar_menu_select.svg"))
+        tabArray.append(createNav(OrderViewController(), title: "订单", image: "tabbar_home", seletImage: "tabbar_home_select.png"))
+        tabArray.append(createNav(MineViewController(), title: "我的", image: "tabbar_home", seletImage: "tabbar_home_select.png"))
         self.viewControllers = tabArray
     }
     
+    //创建导航栏控制器
     func createNav(_ viewController:UIViewController!,title:String!,image:String!,seletImage:String!) -> UINavigationController {
         
-        let item : UITabBarItem = UITabBarItem (title: title, image: UIImage(named: image), selectedImage: UIImage(named: seletImage)?.withRenderingMode(.alwaysOriginal))
+        let _image:UIImage?;
+        let _imageSelect:UIImage?;
+        //加载SVG图片
+        if(image.contains("svg") == true) {
+            let svgImage = SVGKImage.init(named: image);
+            svgImage?.size = .init(width: 20, height: 20)
+            _image = svgImage?.uiImage
+            
+            let svgImageSelect = SVGKImage.init(named: seletImage);
+            svgImageSelect?.size = .init(width: 20, height: 20)
+            _imageSelect = SVGKImage.init(named: seletImage).uiImage.withRenderingMode(.alwaysOriginal)
+        }else {
+            _image =  UIImage(named: image)?.withRenderingMode(.alwaysOriginal)
+            _imageSelect =  UIImage(named: seletImage)?.withRenderingMode(.alwaysOriginal)
+        }
+        
+        
+        
+        
+        let item : UITabBarItem = UITabBarItem (title: title, image: _image, selectedImage: _imageSelect)
         
         viewController.tabBarItem = item
-//        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.red], for:.normal);
-//        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.blue], for:.selected)
+        
         self.tabBar.barTintColor=UIColor.clear;
-                viewController.tabBarItem!.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.blue], for: .normal)
-                viewController.tabBarItem!.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.blue], for: .selected)
+        viewController.tabBarItem!.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor(hexString: "#2b4c7e") as Any], for: .normal)
+        viewController.tabBarItem!.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor(hexString: "#2b4c7e") as Any], for: .selected)
         //导航栏
         let nav = UINavigationController(rootViewController: viewController)
         nav.navigationBar.barTintColor = UIColor.white
